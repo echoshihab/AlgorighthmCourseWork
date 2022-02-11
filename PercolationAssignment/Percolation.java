5761756e7eafd65041f1456cb8cbd73be8073465
echoshihab <echoshihab@gmail.com>
2022-02-11 17:29:52 -0500

    code cleanup

--- PercolationAssignment/Percolation.java
+++ PercolationAssignment/Percolation.java
@@ -4,130 +4,109 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-    private WeightedQuickUnionUF quf;
-    private int[] parallelStrucure;
-    private int virtualTopSiteIndex;
-    private int virtualBottomSiteIndex;
-    private int openSites;
-    private int size;
-    private int qufSize;
+  private final WeightedQuickUnionUF quf;
+  private final int[] parallelStrucure;
+  private final int virtualTopSiteIndex;
+  private final int virtualBottomSiteIndex;
+  private int openSites;
+  private final int size;
+  private final int qufSize;
 
+  public Percolation(int n) {
+    if (n <= 0) {
+      throw new IllegalArgumentException();
+    }
+    size = n;
+    // add 2 for two additional virtual site
+    qufSize = (n * n) + 2;
+    virtualTopSiteIndex = qufSize - 1 - 1;
+    virtualBottomSiteIndex = qufSize - 1;
 
-    public Percolation(int n) {
-        if (n <= 0) {
-            throw new IllegalArgumentException();
-        }
-        size = n;
-        //add 2 for two additional virtual site
-        qufSize = (n * n) + 2;
-        virtualTopSiteIndex = qufSize - 1 - 1;
-        virtualBottomSiteIndex = qufSize - 1;
+    // this parallel structure will be our representation of the grid
+    parallelStrucure = new int[n * n];
+    int parrallelStructureLength = n * n;
 
-        //this parallel structure will be our representation of the grid
-        parallelStrucure = new int[n * n];
-        int parrallelStructureLength = n * n;
+    // lets set all slots to -1 to mark them as closed
+    for (int i = 0; i < parrallelStructureLength; i++) {
+      parallelStrucure[i] = -1;
+    }
+    quf = new WeightedQuickUnionUF(qufSize);
+  }
 
-        // lets set all slots to -1 to mark them as closed
-        for (int i = 0; i < parrallelStructureLength; i++) {
-            parallelStrucure[i] = -1;
-        }
-        quf = new WeightedQuickUnionUF(qufSize);
+  public void open(int row, int col) {
 
+    int site = (size * (row - 1)) + col;
+    int siteIndexValue = site - 1;
+    // only open site if the site is closed
+    if (parallelStrucure[siteIndexValue] == -1) {
+      // open site
+      parallelStrucure[siteIndexValue] = siteIndexValue;
+      openSites += 1;
+      // if this is on top row connect them to virtual site
+      if (site <= size) {
+        quf.union(virtualTopSiteIndex, siteIndexValue);
+      }
+      // if this is bottom row, connect it to virtual bottom site
+      if (site > (size * size) - size) {
+        quf.union(virtualBottomSiteIndex, siteIndexValue);
+      }
     }
 
-    public void open(int row, int col) {
+    // lets check if adjacent sites exist
+    boolean rightSideExists = site % size != 0;
+    boolean leftSideExists = site % size != 1;
+    boolean topSideExists = site > size;
+    boolean bottomSideExists = site <= (size * size - size);
 
-        int site = (size * (row - 1)) + col;
-        System.out.println("Site: " + site);
-        int siteIndexValue = site - 1;
-        System.out.println("SiteIndexValue: " + siteIndexValue);
-        //only open site if the site is closed
-        if (parallelStrucure[siteIndexValue] == -1) {
-            //open site
-            parallelStrucure[siteIndexValue] = siteIndexValue;
-            openSites += 1;
-            System.out.println("opening site");
-            //if this is on top row connect them to virtual site
-            if (site <= size) {
-                quf.union(virtualTopSiteIndex, siteIndexValue);
-                System.out.println("connecting to virtual top site");
-            }
-            //if this is bottom row, connect it to virtual bottom site
-            if (site > (size * size) - size) {
-                quf.union(virtualBottomSiteIndex, siteIndexValue);
-                System.out.println("connecting to virtual bottom site");
-            }
-
-        }
-
-        //lets check if adjacent sites exist
-        boolean rightSideExists = site % size != 0;
-        boolean leftSideExists = site % size != 1;
-        boolean topSideExists = site > size;
-        boolean bottomSideExists = site <= (size * size - size);
-
-        //connect if right side is open
-        if (rightSideExists && parallelStrucure[siteIndexValue + 1] != -1) {
-            quf.union(siteIndexValue, siteIndexValue + 1);
-        }
-        //connect if left side is open
-        if (leftSideExists && parallelStrucure[siteIndexValue - 1] != -1) {
-            quf.union(siteIndexValue, siteIndexValue - 1);
-        }
-        //connect if top side is open
-        if (topSideExists && parallelStrucure[siteIndexValue - size] != -1) {
-            quf.union(siteIndexValue, siteIndexValue - size);
-        }
-        //connect if bottom side is open
-        if (bottomSideExists && parallelStrucure[siteIndexValue + size] != -1) {
-            quf.union(siteIndexValue, siteIndexValue + size);
-
-
-        }
-
-
+    // connect if right side is open
+    if (rightSideExists && parallelStrucure[siteIndexValue + 1] != -1) {
+      quf.union(siteIndexValue, siteIndexValue + 1);
     }
-
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-        int site = (size * (row - 1)) + col;
-        int siteIndexValue = site - 1;
-        if (parallelStrucure[siteIndexValue] != -1) {
-            return true;
-        }
-        return false;
-
+    // connect if left side is open
+    if (leftSideExists && parallelStrucure[siteIndexValue - 1] != -1) {
+      quf.union(siteIndexValue, siteIndexValue - 1);
     }
-
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-        int site = (size * (row - 1)) + col;
-        int siteIndexValue = site - 1;
-
-        //A full site is an open site that can be
-        // connected to an open site in the top
-        // row via a chain of neighboring (left, right, up, down)
-        // open sites.
-        if (quf.find(siteIndexValue) == quf.find(virtualTopSiteIndex))
-            return true;
-        return false;
+    // connect if top side is open
+    if (topSideExists && parallelStrucure[siteIndexValue - size] != -1) {
+      quf.union(siteIndexValue, siteIndexValue - size);
     }
-
-    // returns the number of open sites
-    public int numberOfOpenSites() {
-        return openSites;
+    // connect if bottom side is open
+    if (bottomSideExists && parallelStrucure[siteIndexValue + size] != -1) {
+      quf.union(siteIndexValue, siteIndexValue + size);
     }
+  }
 
-    // does the system percolate?
-    public boolean percolates() {
-        System.out.println(quf.find(virtualTopSiteIndex));
-        System.out.println(quf.find(virtualBottomSiteIndex));
-        return quf.find(virtualTopSiteIndex) == quf.find(virtualBottomSiteIndex);
-    }
+  // is the site (row, col) open?
+  public boolean isOpen(int row, int col) {
+    int site = (size * (row - 1)) + col;
+    int siteIndexValue = site - 1;
+    return parallelStrucure[siteIndexValue] != -1;
+  }
 
-    // test client (optional)
-    public static void main(String[] args) {
-        PercolationVisualizer.main(args);
-    }
+  // is the site (row, col) full?
+  public boolean isFull(int row, int col) {
+    int site = (size * (row - 1)) + col;
+    int siteIndexValue = site - 1;
 
+    // A full site is an open site that can be
+    // connected to an open site in the top
+    // row via a chain of neighboring (left, right, up, down)
+    // open sites.
+    return quf.find(siteIndexValue) == quf.find(virtualTopSiteIndex);
+  }
+
+  // returns the number of open sites
+  public int numberOfOpenSites() {
+    return openSites;
+  }
+
+  // does the system percolate?
+  public boolean percolates() {
+    return quf.find(virtualTopSiteIndex) == quf.find(virtualBottomSiteIndex);
+  }
+
+  // test client (optional)
+  public static void main(String[] args) {
+    PercolationVisualizer.main(args);
+  }
 }
--- PercolationAssignment/PercolationStats.java
+++ PercolationAssignment/PercolationStats.java
@@ -1,83 +1,83 @@
 package com.company;
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
-    private int nval;
-    private int trialsVal;
-    private int[] openSitesAtPerc;
-    private double meanValue;
-    private double standardDevValue;
-    private double lowConfidence;
-    private double highConfidence;
-    // perform independent trials on an n-by-n grid
-    public PercolationStats(int n, int trials)
-    {
-        if(n <= 0 || trials <= 0)
-        {
-            throw new IllegalArgumentException();
+  private int nval;
+  private int trialsVal;
+  private double[] percolationThreshold;
+  private double meanValue;
+  private double standardDevValue;
+  private double lowConfidence;
+  private double highConfidence;
+  // perform independent trials on an n-by-n grid
+  public PercolationStats(int n, int trials) {
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException();
+    }
+    nval = n;
+    trialsVal = trials;
+    percolationThreshold = new double[trials];
+
+    int trialNum = 0;
+    for (int i = 0; i < trials; i++) {
+      Percolation perc = new Percolation(n);
+      while (true) {
+        // randomly open site
+        int randomSite = StdRandom.uniform(n * n);
+        int reminder = randomSite % nval;
+        int divResult = randomSite / nval;
+        int col, row;
+        if (reminder != 0) {
+          row = divResult + 1;
+          col = reminder;
+        } else {
+          row = divResult;
+          col = nval;
         }
-        nval = n;
-        trialsVal = trials;
-        openSitesAtPerc = new int[trials];
-        Percolation perc = new Percolation(n);
-
-        int trialNum = 0;
-        for(int i=0; i<trials; i++) {
-            while(!perc.percolates() ){
-                //randomly open site
-                int randomSite = StdRandom.uniform(n*n);
-                int reminder =  randomSite % nval;
-                int divResult = randomSite / nval;
-                int col, row;
-                if(reminder != 0) {
-                    row = divResult + 1;
-                    col = reminder;
-                }  else {
-                    row = divResult;
-                    col = nval;
-                }
-                perc.open(col,row);
-            }
-            openSitesAtPerc[i] = perc.numberOfOpenSites();
+        perc.open(col, row);
+        if (perc.percolates()) {
+          break;
         }
+      }
+      percolationThreshold[i] = perc.numberOfOpenSites() / (double) (n * n);
+    }
+  }
 
-    }
+  // sample mean of percolation threshold
+  public double mean() {
+    meanValue = StdStats.mean(percolationThreshold);
+    return meanValue;
+  }
+  // sample standard deviation of percolation threshold
+  public double stddev() {
+    standardDevValue = StdStats.stddev(percolationThreshold);
+    return standardDevValue;
+  }
+  // low endpoint of 95% confidence interval
+  public double confidenceLo() {
+    lowConfidence = meanValue - ((1.96 * standardDevValue) / Math.sqrt(trialsVal));
+    return lowConfidence;
+  }
+  // high endpoint of 95% confidence interval
+  public double confidenceHi() {
+    highConfidence = meanValue + ((1.96 * standardDevValue) / Math.sqrt(trialsVal));
+    return highConfidence;
+  }
+  // test client (see below)
+  public static void main(String[] args) {
+    int n = Integer.valueOf(args[0]);
+    int trials = Integer.valueOf(args[1]);
 
-    // sample mean of percolation threshold
-    public double mean()
-    {
-        meanValue = StdStats.mean(openSitesAtPerc);
-        return meanValue;
-
-    }
-    // sample standard deviation of percolation threshold
-    public double stddev()
-    {
-        standardDevValue = StdStats.stddev(openSitesAtPerc);
-        return standardDevValue;
-    }
-    // low endpoint of 95% confidence interval
-    public double confidenceLo()
-    {
-        lowConfidence = meanValue - ((1.96 * standardDevValue)/ Math.sqrt(trialsVal));
-        return lowConfidence;
-    }
-    // high endpoint of 95% confidence interval
-    public double confidenceHi()
-    {
-        highConfidence = meanValue - ((1.96 * standardDevValue)/ Math.sqrt(trialsVal));
-        return highConfidence;
-    }
-    // test client (see below)
-    public static void main(String[] args)
-    {
-        int n = Integer.valueOf(args[0]);
-        int trials = Integer.valueOf(args[1]);
-
-        PercolationStats percStat = new PercolationStats(n, trials);
-        System.out.println("mean = "+ percStat.mean());
-        System.out.println("stddev = "+ percStat.stddev());
-        System.out.println("95% Confidence interval = ["+ percStat.confidenceHi() + "," + percStat.confidenceLo() + "]");
-    }
+    PercolationStats percStat = new PercolationStats(n, trials);
+    System.out.println("mean = " + percStat.mean());
+    System.out.println("stddev = " + percStat.stddev());
+    System.out.println(
+        "95% Confidence interval = ["
+            + percStat.confidenceLo()
+            + ","
+            + percStat.confidenceHi()
+            + "]");
+  }
 }
